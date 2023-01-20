ecfdd3f6e4b66ba10515b0d50e86a4e5390cf3ad
ngough87 <74070200+ngough87@users.noreply.github.com>
2023-01-20 17:30:52 -0600

    Create Boo/REST preojct, add JPA project dependency

---
+++ thOmaha/src/main/java/com/skilldistillery/theomaha/ThOmahaApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.theomaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThOmahaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThOmahaApplication.class, args);
+	}
+
+}
